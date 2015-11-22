c5b515ff26a874d78b1d58e4db95926f7c3a5d93
volfor <volfor@mail.ua>
2015-11-22 14:01:16 +0200

    No internet connection check.

--- app/src/main/java/com/vtrifonov/weatherapp/activities/MainActivity.java
+++ app/src/main/java/com/vtrifonov/weatherapp/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.vtrifonov.weatherapp.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
@@ -9,10 +11,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vtrifonov.weatherapp.R;
 import com.vtrifonov.weatherapp.fragments.DetailsFragment;
 import com.vtrifonov.weatherapp.fragments.ListFragment;
+import com.vtrifonov.weatherapp.fragments.NoConnectionFragment;
 import com.vtrifonov.weatherapp.model.RetrieveWeatherTask;
 import com.vtrifonov.weatherapp.model.WeatherGetter;
 import com.vtrifonov.weatherapp.model.WeatherObject;
@@ -21,9 +25,10 @@ public class MainActivity extends BaseActivity implements WeatherGetter, ListFra
 
     private static ListFragment listFragment;
     private static DetailsFragment detailsFragment;
+    private static NoConnectionFragment noConnectionFragment;
 
-    private static String city;
-    private static String country;
+    private String city;
+    private String country;
     private TextView currCity;
     private int position;
     private ProgressBar spinner;
@@ -37,6 +42,8 @@ public class MainActivity extends BaseActivity implements WeatherGetter, ListFra
 
         listFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.list_fragment);
         detailsFragment = (DetailsFragment) getSupportFragmentManager().findFragmentById(R.id.details_fragment);
+        noConnectionFragment = (NoConnectionFragment)
+                getSupportFragmentManager().findFragmentById(R.id.no_connection_fragment);
         currCity = (TextView) findViewById(R.id.current_city);
         spinner = (ProgressBar) findViewById(R.id.progressBar);
 
@@ -51,9 +58,13 @@ public class MainActivity extends BaseActivity implements WeatherGetter, ListFra
         }
 
         if (savedInstanceState == null) {
-            spinner.setVisibility(View.VISIBLE);
-            RetrieveWeatherTask task = new RetrieveWeatherTask(this);
-            task.execute(city, country);
+            if (isNetworkConnected()) {
+                spinner.setVisibility(View.VISIBLE);
+                RetrieveWeatherTask task = new RetrieveWeatherTask(this);
+                task.execute(city, country);
+            } else {
+                noConnectionFragment.getView().setVisibility(View.VISIBLE);
+            }
         } else {
             position = savedInstanceState.getInt("position");
             if (position >= 0) {
@@ -113,6 +124,11 @@ public class MainActivity extends BaseActivity implements WeatherGetter, ListFra
         }
     }
 
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null;
+    }
+
     public void checkDefaults() {
         city = SettingsActivity.getDefaults("city", this);
         country = SettingsActivity.getDefaults("country", this);
@@ -129,9 +145,14 @@ public class MainActivity extends BaseActivity implements WeatherGetter, ListFra
     }
 
     public void refresh() {
-        spinner.setVisibility(View.VISIBLE);
-        RetrieveWeatherTask task = new RetrieveWeatherTask(this);
-        task.execute(city, country);
+        if (isNetworkConnected()) {
+            spinner.setVisibility(View.VISIBLE);
+            RetrieveWeatherTask task = new RetrieveWeatherTask(this);
+            task.execute(city, country);
+            noConnectionFragment.getView().setVisibility(View.GONE);
+        } else {
+            Toast.makeText(this, "No Internet", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/vtrifonov/weatherapp/fragments/NoConnectionFragment.java
@@ -0,0 +1,23 @@
+package com.vtrifonov.weatherapp.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vtrifonov.weatherapp.R;
+
+public class NoConnectionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_no_connection, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.setVisibility(View.GONE);
+    }
+}
